cd96023e340ecec257ec7c2d93d62b16eea0e265
Noxzyous <154869918+Noxzyous@users.noreply.github.com>
2024-01-18 02:35:21 +0000

    aaaaaaaaaahhhhh

--- calc.py
+++ calc.py
@@ -8,6 +8,29 @@ calc = Flask(__name__)
 def home_page():
     return render_template('form.html')
 
-if __name__ == '__main__':
-    calc.run()
+@calc.route('/result', methods=['GET','POST'])
+def math_is():
+    if request.method=='POST':
+        num1 = request.form.get("num1", type=int)
+        num2 = request.form.get("num2", type=int)
+        yor_job = request.form.get("yor_job")
 
+        if(yor_job == 'Addition'):
+            result = num1+num2
+        elif(yor_job == 'Subtraction'):
+            result = num1-num2
+        elif(yor_job == 'Multiplication'):
+            result = num1*num2
+        elif(yor_job == 'Division'):
+            if(num1==0 and num2==0):
+                result = 0
+            else:
+                result = num1/num2
+        else:
+            result = 0
+        output = result
+        return render_template('result.html', output=output)
+
+
+if __name__ == '__main__':
+    calc.run(debug=True)
